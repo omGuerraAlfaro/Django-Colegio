87ecce485fbb79d7a29b70d3a303d0a4df886217
omGuerraAlfaro <om.guerra@duocuc.cl>
2022-06-19 10:20:17 -0400

    HTML

--- equipo/static/equipo/js/ScriptDjango.js
+++ equipo/static/equipo/js/ScriptDjango.js
@@ -1,5 +1,15 @@
-$(document).ready(function() {
-    $("#eliminarDoc").click(function() {
-    confirm('Do you really want to remove/delete the user ?')
+const btnEliminacion = document.querySelectorAll('.btnEliminar');
+
+(function(){
+
+    btnEliminacion.forEach(btn => {
+        btn.addEventListener('click',function(e){
+            let confirmacion=confirm("¿Confirma la eliminación del Docente?");
+            if (!confirmacion){
+                e.preventDefault();
+            }
+        })
     });
-});
+
+
+})
