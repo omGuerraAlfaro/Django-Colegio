79b164a022070be7aa7863c508d299b404f75d74
Omar Guerra Alfaro <om.guerra@duocuc.cl>
2022-06-29 22:49:24 -0400

    login

--- rest_taller/urls.py
+++ rest_taller/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import lista_taller, detalle_taller
-from .viewsLogin import loginApi
+from .viewsLogin import f_login
 
 urlpatterns = [
     path('lista_taller',lista_taller,name="lista_taller"),
     path('detalle_taller/<sku>',detalle_taller,name="detalle_taller"),
-    path('loginApi',loginApi,name="loginApi"),
+    path('login',f_login,name='login_html'),
 ]
